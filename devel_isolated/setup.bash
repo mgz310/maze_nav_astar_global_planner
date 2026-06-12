f14b9caccca8e5519508866327accc0ad9535c74
mgz310 <3299037945@qq.com>
2026-06-12 22:02:10 +0800

    Delete devel_isolated directory

--- devel_isolated/setup.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# generated from catkin.builder Python module
-
-. "/home/cai/first_nav/devel_isolated/maze_nav/setup.bash"
--- devel_isolated/setup.zsh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-# generated from catkin.builder Python module
-
-. "/home/cai/first_nav/devel_isolated/maze_nav/setup.zsh"
